8d617b2889778f412a18f1224f593ad99d40e346
Amaan Cheval <amaan.cheval@gmail.com>
2015-08-06 15:53:57 +0530

    Added MIDI.js

--- conway/conway.js
+++ conway/conway.js
@@ -41,6 +41,22 @@
                 grid[i][j] = 0;
             }
         }
+        MIDI.loadPlugin({
+            soundfontUrl: "./soundfont/",
+            instrument: "acoustic_grand_piano",
+            onprogress: function(state, progress) {
+                console.log(state, progress);
+            },
+            onsuccess: function() {
+                var delay = 0; // play one note every quarter second
+                var note = 50; // the MIDI note
+                var velocity = 127; // how hard the note hits
+                // play the note
+                MIDI.setVolume(0, 127);
+                MIDI.noteOn(0, note, velocity, delay);
+                MIDI.noteOff(0, note, delay + 0.75);
+            }
+        });
     };
 
     var getNeighbours = function(row, column) {
